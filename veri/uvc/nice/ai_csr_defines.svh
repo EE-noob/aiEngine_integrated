558c127cb103ce156fca452ae98ab673fb5ca378
unknown <2169217139@qq.com>
2025-11-24 01:10:27 +0800

    auto commit

--- veri/uvc/nice/ai_csr_defines.svh
+++ veri/uvc/nice/ai_csr_defines.svh
@@ -2,23 +2,28 @@
 `define AI_CSR_DEFINES_SVH
 
 // CSR Addresses
+// Pointer Registers
 `define ADDR_MULT_LHS_PTR        12'h7C0
 `define ADDR_MULT_RHS_PTR        12'h7C1
 `define ADDR_MULT_DST_PTR        12'h7C2
 `define ADDR_MULT_BIAS_PTR       12'h7C3
-`define ADDR_MULT_LHS_OFFSET     12'h7C4
-`define ADDR_MULT_RHS_OFFSET     12'h7C5
-`define ADDR_MULT_DST_OFFSET     12'h7C6 // Output Zero Point
-`define ADDR_MULT_DST_MULT       12'h7C7
-`define ADDR_MULT_DST_SHIFT      12'h7C8
-`define ADDR_MULT_LHS_ROWS       12'h7C9 // M
-`define ADDR_MULT_RHS_ROWS       12'h7CA // K
-`define ADDR_MULT_RHS_COLS       12'h7CB // N
-`define ADDR_MULT_LHS_STRIDE     12'h7CC
-`define ADDR_MULT_RHS_STRIDE     12'h7CD
-`define ADDR_MULT_DST_STRIDE     12'h7CE
-`define ADDR_MULT_ACT_MIN        12'h7CF
-`define ADDR_MULT_ACT_MAX        12'h7D0
+
+// Dimension & Stride Registers
+`define ADDR_MULT_LHS_ROWS       12'h7C4 // K (A Rows)
+`define ADDR_MULT_RHS_COLS       12'h7C5 // N (B Rows / Inner Dim)
+`define ADDR_MULT_RHS_ROWS       12'h7C6 // M (B Cols / Output Channels)
+`define ADDR_MULT_DST_STRIDE     12'h7C7 // C Row Stride
+`define ADDR_MULT_LHS_STRIDE     12'h7C8 // A Row Stride
+`define ADDR_MULT_RHS_STRIDE     12'h7C9 // B Col Stride
+
+// Quantization & Activation Registers
+`define ADDR_MULT_LHS_OFFSET     12'h7CA // A Zero Point
+`define ADDR_MULT_RHS_OFFSET     12'h7CB // B Zero Point
+`define ADDR_MULT_DST_OFFSET     12'h7CC // Output Zero Point
+`define ADDR_MULT_DST_MULT       12'h7CD // Multiplier (Scalar or Pointer)
+`define ADDR_MULT_DST_SHIFT      12'h7CE // Shift (Scalar or Pointer)
+`define ADDR_MULT_ACT_MIN        12'h7CF // Activation Min
+`define ADDR_MULT_ACT_MAX        12'h7D0 // Activation Max
 
 // Instruction Encodings
 `define NICE_CUSTOM_1            7'b0101011
